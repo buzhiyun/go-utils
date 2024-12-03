86459e1fae3931d4623f76a5e1f6c79f40c59825
zhouyang <zhouyang@7net.cc>
2024-12-03 08:42:52 +0800

    修正前景色和背景色的显示，不强制指定背景颜色

--- str/color.go
+++ str/color.go
@@ -22,43 +22,61 @@ import "fmt"
 //  7  反白显示
 //  8  不可见
 
-type ColorFont int
-type ColorBG int
+type ColorFont string
+type ColorBG string
+type FontStyle int
 
 const (
+	FontDefault      FontStyle = 0 + iota // 默认
+	FontBold                              // 高亮
+	FontFuzzy                             // **模糊
+	FontItalics                           // **斜体
+	FontUnderline                         // 下划线
+	FontSlowBlinking                      //慢闪烁
+	FontFastBlinking                      // **快闪烁
+	FontReverseColor                           // 反白显示
+	FontHiding                            // 不可见
+
 	// 字体本身
-	BlackFont  ColorFont = 30 + iota // 黑色
-	RedFont                          // 红色
-	GreenFont                        // 绿色
-	YellowFont                       // 黄色
-	BlueFont                         // 蓝色
-	PurpleFont                       // 紫色
-	CyanFont                         // 青色
-	WhiteFont                        // 白色
+	BlackFont  ColorFont = ";30" // 黑色
+	RedFont    ColorFont = ";31" // 红色
+	GreenFont  ColorFont = ";32" // 绿色
+	YellowFont ColorFont = ";33" // 黄色
+	BlueFont   ColorFont = ";34" // 蓝色
+	PurpleFont ColorFont = ";35" // 紫色
+	CyanFont   ColorFont = ";36" // 青色
+	WhiteFont  ColorFont = ";37" // 白色
 
 	// 背景色
-	BlackBG  ColorBG = 40 + iota
-	RedBG            // 红色
-	GreenBG          // 绿色
-	YellowBG         // 黄色
-	BlueBG           // 蓝色
-	PurpleBG         // 紫色
-	CyanBG           // 青色
-	WhiteBG          // 白色
+	BlackBG  ColorBG = ";40"
+	RedBG    ColorBG = ";41" // 红色
+	GreenBG  ColorBG = ";42" // 绿色
+	YellowBG ColorBG = ";43" // 黄色
+	BlueBG   ColorBG = ";44" // 蓝色
+	PurpleBG ColorBG = ";45" // 紫色
+	CyanBG   ColorBG = ";46" // 青色
+	WhiteBG  ColorBG = ";47" // 白色
 )
 
 /*
 *
 获取带有颜色的字符串 ，windows 不能用
 */
-func ColorfulString(str string, fontColor ColorFont, backgroudColor ColorBG) string {
-	return fmt.Sprintf("\033[1;%v;%vm%s\033[0m", fontColor, backgroudColor, str)
+func ColorfulString(str string, fontStyle FontStyle, fontColor ColorFont, backgroudColor ...ColorBG) string {
+	if len(backgroudColor) > 0 {
+		return fmt.Sprintf("\033[%v%s%sm%s\033[0m", fontStyle, fontColor, backgroudColor[0], str)
+	}
+	return fmt.Sprintf("\033[%v%sm%s\033[0m", fontStyle, fontColor, str)
 }
 
 /*
 *
 显示带有颜色的字符串 ，windows 不能用
 */
-func ColorfulPrint(str string, fontColor ColorFont, backgroudColor ColorBG) {
-	fmt.Printf("\033[1;%v;%vm%s\033[0m", fontColor, backgroudColor, str)
+func ColorfulPrint(str string, fontStyle FontStyle, fontColor ColorFont, backgroudColor ...ColorBG) {
+	if len(backgroudColor) > 0 {
+		fmt.Printf("\033[%v%s%sm%s\033[0m", fontStyle, fontColor, backgroudColor[0], str)
+		return
+	}
+	fmt.Printf("\033[%v%sm%s\033[0m", fontStyle, fontColor, str)
 }
